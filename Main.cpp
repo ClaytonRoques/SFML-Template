6e112a1cd826aa691af01401450d24c8ddfb0c95
Clayton Roques <claytonroques1@gmail.com>
2022-01-21 09:48:42 -0800

    Setup game class

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,29 @@
-
-#include "SFML/Graphics.hpp"
-#include "SFML/Window.hpp"
 #include "iostream"
+#include "Game.h"
+
 using std::cout;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(300, 200), "Test");
-
-	// run the program as long as the window is open
-	while (window.isOpen())
+	//Init Game engine
+	Game game;
+	// game loop
+	while (game.running())
 	{
-		// check all the window's events that were triggered since the last iteration of the loop
-		sf::Event e;
-		while (window.pollEvent(e))
-		{
-			switch (e.type)
-			{
-			case sf::Event::Closed:
-				cout << "Close command received\n";
-				window.close();
-			case sf::Event::LostFocus:
-				cout << "Lost focus\n";
-			case sf::Event::GainedFocus:
-				cout << "Gained focus\n";
-			}
-		}
+		
+
+		//Update
+		game.update();
+		//Render
+		game.render();
+		//Draw game
+
+
+
+
+
+
+		/*
 
 		// clear the window with black color
 		window.clear(sf::Color::Black);
@@ -35,13 +33,14 @@ int main()
 		shape.setFillColor(sf::Color(0, 0, 255));
 
 		sf::CircleShape shape2(50.f);
-		shape2.setFillColor(sf::Color(0, 0, 255));
+		shape2.setFillColor(sf::Color::Cyan);
 		shape2.setOrigin(-97.f, 0.f);
 		window.draw(shape);
 		window.draw(shape2);
 
 		// end the current frame
 		window.display();
+		*/
 	}
 	return 0;
 }
